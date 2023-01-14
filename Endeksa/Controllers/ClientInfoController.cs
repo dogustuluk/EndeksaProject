53820544f7d7ad9a72962b9937d49589c9fbd1cd
dogustuluk <dogus.tuluk@gmail.com>
2023-01-14 16:43:18 +0300

    Add project files.

---
+++ Endeksa/Controllers/ClientInfoController.cs
@@ -0,0 +1,85 @@
+﻿using Endeksa.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Net;
+
+namespace Endeksa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientInfoController : ControllerBase
+    {
+        [HttpGet]
+        //Authorize
+        [SwaggerOperation(
+            Summary = "Get User IP and location",
+            Description = "Get User and location by accessing an external API",
+            OperationId = "GetUserAndLocation",
+            Tags = new[] {"IP"}
+            )]
+        public ActionResult<UserLocation> GetIP()
+        {
+            //kullanıcının ip adresi alınır.
+            string ip = GetUserIP();
+            //IP adresinin kullanılarak istekte bulunan kullanıcının konum bilgileri alınır.
+            string location = GetLocation(ip);
+
+            return Ok(new UserLocation { IP = ip, Location = location });
+        }
+        /// <summary>
+        /// Gelen isteğin IP adresini api'ye bağlanarak bulan method. Json formatında bir değer alır ve geriye string türünde değer döndürür. Gelen değer GetIP metodunda çalıştırılır.
+        /// </summary>
+        /// <returns></returns>
+        private string GetUserIP()
+        {
+            string ip = "";
+            try
+            {
+                //IP adresi için api çağrısı yapılır.
+                string apiUrl = "http://api.ipstack.com/check?access_key=403961d7a38018e6c9392289a30bd249\r\n";
+                var json = new WebClient().DownloadString( apiUrl );
+                var data = JObject.Parse( json );
+
+                //IP adresi alınır
+                ip = data["ip"].ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine( ex.Message );
+            }
+
+            return ip;
+        }
+        /// <summary>
+        /// Gelen isteğin sahip olduğu ip adresine bağlı olarak geriye ülke ve şehir bilgisini döndürür. Json formatında bir değer alır ve geriye string türünde değer döndürür. Gelen değer GetIP metodunda çalıştırılır.
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        private string GetLocation(string ip)
+        {
+            string location = "";
+            try
+            {
+                //Konum bilgilerinin ip adresi kullanılarak gelmesi için api çağrısı yapılır.
+                string apiUrl = $"http://api.ipstack.com/{ip}?access_key=403961d7a38018e6c9392289a30bd249\r\n";
+                var json = new WebClient().DownloadString( apiUrl );
+                var data = JObject.Parse( json );
+
+                //Şehir ve ülke bilgileri alınır
+                string city = data["city"].ToString();
+                string country = data["country_name"].ToString();
+
+                location = $"{city}, {country}";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return location;
+        }
+    }
+}
---
+++ Endeksa/Models/UserLocation.cs
@@ -0,0 +1,8 @@
+﻿namespace Endeksa.Models
+{
+    public class UserLocation
+    {
+        public string IP { get; set; }
+        public string Location { get; set; }
+    }
+}
