ba1a0827f0ab743ddf8caae5d5b7d9a0defa70bd
dogustuluk <dogus.tuluk@gmail.com>
2023-01-14 23:45:56 +0300

    updated

--- Endeksa/Startup.cs
+++ Endeksa/Startup.cs
@@ -1,3 +1,4 @@
+using Endeksa.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,10 @@ namespace Endeksa
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(serviceProvider => new ConnectionFactory { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")), DispatchConsumersAsync= true });
+
+            services.AddSingleton<RabbitMQClientService>();
+            
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
